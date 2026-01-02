2f39126caeb5e728ad8070a690864675d2c119f6
hindyhh <hhamburg@mail.yu.edu>
2026-01-02 01:12:25 -0500

    added y and r

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <ctype.h>
 
-// pass through all the spaces returning the next non-space character
-static int skip_space(int c) {
+// pass through all the spaces, returning the next non-space character
+int skip_space(int c) {
     while (isspace(c)) {
         c = getchar();
     }
@@ -11,8 +11,19 @@ static int skip_space(int c) {
 }
 
 // returns if chars_read is within the width amount
-static int check_width(int width, int chars_read) {
+int check_width(int width, int chars_read) {
     return width == 0 || chars_read < width;
+}
+
+int roman_values(char c) {
+    if (c == 'I'){ return 1;}
+    if (c == 'V'){ return 5;}
+    if (c == 'X'){ return 10;}
+    if (c == 'L'){ return 50;}
+    if (c == 'C'){ return 100;}
+    if (c == 'D'){ return 500;}
+    if (c == 'M'){ return 1000;}
+    return -1;
 }
 
 int my_scanf(const char *format, ...) {
@@ -96,6 +107,7 @@ int my_scanf(const char *format, ...) {
                 case 'c': {
                     // get the char pointer from the list and store the answer
                     char *out = va_arg(args, char *);
+                    c = skip_space(c);
                     *out = (char)c;
                     count++;
                     c = getchar();
@@ -178,9 +190,62 @@ int my_scanf(const char *format, ...) {
                     count++;
                     break;
                 }
+
+                case 'y': {
+                    // get the int pointer from the list
+                    char *out = va_arg(args, char*);
+                    int value = 0;
+                    int chars_read = 0;
+
+                    c = skip_space(c);
+
+                    while (isdigit(c) && check_width(width, chars_read)) {
+                        // add the next digit after the previous ones
+                        value = value * 10 + c - '0';
+                        chars_read++;
+                        c = getchar();
+                    }
+                    // store the answer
+                    *out = (char) value;
+                    count++;
+                    break;
+                }
+
+                case 'r': {
+                    int *out = va_arg(args, int *);
+                    int chars_read = 0;
+                    int total = 0;
+                    int prev = 0;
+                    int curr;
+                    int read_any = 0;
+
+                    c = skip_space(c);
+
+                    // while we are still in the string
+                    while (c != EOF && roman_values(c) != -1 &&
+                        check_width(width, chars_read)) {
+                            read_any = 1;
+                            curr = roman_values(c);
+                            c = getchar();
+
+                            if (curr > prev) {
+                                total += curr - 2 * prev;
+                            } else {
+                                total += curr;
+                            }
+                            prev = curr;
+                            chars_read++;
+                    }
+                    if (!read_any) {
+                        break;
+                    }
+                    *out = total;
+                    count++;
+                    break;
+                }
             }
         }else {
-            // if the input and format match move on
+            // if the input and format match, move on
             if (c == *format) {
                 c = getchar();
             }
@@ -199,24 +264,35 @@ int main() {
     char s[100];
     char c;
     int result;
+    char y;
+    int r;
 
     printf("-----Test myscanf-------\n");
 
-    printf("Enter an integer: ");
+    printf("integer: ");
     result = my_scanf("%d", &i);
-    printf("Result count: %d | Values: i=%d\n\n", result, i);
+    printf("count: %d\nvalues: i=%d\n\n", result, i);
 
-    printf("Enter a string and char: ");
+    printf("string and char: ");
     result = my_scanf("%s %c", s, &c);
-    printf("Result count: %d | Values: s=%s, c=%c\n\n", result, s, c);
+    printf("count: %d\nvalues: s=%s, c=%c\n\n", result, s, c);
 
-    printf("Enter a hex number: ");
+    printf("hex number: ");
     result = my_scanf("%x", &x);
-    printf("Result count: %d | Values: x=%x\n\n", result, x);
+    printf("count: %d\nvalues: x=%x\n\n", result, x);
 
-    printf("Enter a float: ");
+    printf("float: ");
     result = my_scanf("%f", &f);
-    printf("Result count: %d | Values: f=%f\n\n", result, f);
+    printf("count: %d\nvalues: f=%f\n\n", result, f);
+
+    printf("ascii: ");
+    result = my_scanf("%y", &y);
+    printf("count: %d\nvalues: y=%c\n\n", result, y);
+
+    printf("Roman Numerals: ");
+    result = my_scanf("%r", &r);
+    printf("count: %d\nvalue: %d\n\n", result, r);
+
 
     return 0;
 }
